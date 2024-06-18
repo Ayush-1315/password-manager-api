598cbf1c1de530a55180e15c1c76df957d91fc40
Ayush Raj <ayush.raj138@gmail.com>
2024-06-19 01:27:35 +0530

    fix- fixed signup route

--- middlewares/users.middleware.js
+++ middlewares/users.middleware.js
@@ -1,21 +1,35 @@
 // Signup credential Validator
 
-const validateSignupMiddleware=(req,res,next)=>{
-    const {username, password,email}=req.body;
-    if(username,email,password){
-        if(email.includes('.com')&&email.includes('@'))
-            if(password.length>=8) 
-                next();
-            else 
-            res.status(400).json({message:'Password should be of 8 characters minimum'})
-        else
-        res.status(400).json({message:'Enter a valid email address'})
-    }
-    else{
-        res.status(400).json({message:'Username, Email and Password are minimum required fields'})
-    }
-}
+const validateSignupMiddleware = (req, res, next) => {
+  const { username, password, email } = req.body;
 
-module.exports={
-    validateSignupMiddleware
-}
+  if (
+    username.trim() &&
+    email.trim() &&
+    password.trim() &&
+    typeof username === "string" &&
+    typeof password === "string" &&
+    typeof email === "string"
+  ) {
+    if (email.includes(".com") && email.includes("@"))
+      if (password.length >= 8) {
+        req.body.username = username.trim();
+        req.body.passwod = password.trim();
+        req.body.email = email.trim();
+        next();
+      } else
+        res
+          .status(400)
+          .json({ message: "Password should be of 8 characters minimum" });
+    else res.status(400).json({ message: "Enter a valid email address" });
+  } else {
+    res.status(400).json({
+      message: "Username, Email and Password are minimum required fields",
+    });
+  }
+};
+
+module.exports = {
+  validateSignupMiddleware,
+
+};
