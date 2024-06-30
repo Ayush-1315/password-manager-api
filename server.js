7dcf8b2827876289852c522ac133b89c24737063
Ayush Raj <ayush.raj138@gmail.com>
2024-07-01 02:26:46 +0530

    fix masterRouter

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
 require("./db");
-const express=require('express');
-const cors=require('cors');
-const helmet=require('helmet'); 
-const bodyParser=require('body-parser')
-const app=express();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const app = express();
 
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 
-const {userRouter}=require('./routers/auth.router');
-const { addPasswordToUserService } = require("./controllers/passwordController.controller");
+const { userRouter } = require("./routers/auth.router");
 const passwordRouter = require("./routers/password.router");
+const masterRouter = require("./routers/master.router");
 
-app.listen(PORT,()=>{
- console.log(`Server is running on PORT: ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
 
-app.get('/',(req,res)=>{
-    res.send("You are Anzen Server")
-})
+app.get("/", (req, res) => {
+    res.send("You are using Anzen Server");
+});
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
-app.use('/password-manager',userRouter);
-app.use('/passwords',passwordRouter);
+app.use("/password-manager", userRouter);
+app.use("/passwords", passwordRouter);
+app.use("/master", masterRouter);
 //Global Error Handler
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     console.log(err.stack);
-    res.status(500).json({message:'Internal Server Error'});
-})
+    res.status(500).json({ message: "Internal Server Error" });
+});
 
 //Global 404 Handler
-app.use((req,res)=>{
-    res.status(404).json({message:'Route not found'});
-})
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
